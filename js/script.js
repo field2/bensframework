5828a8a703c5e8b79d8c4cf2ab70037aa9a2afdc
field2 <bendunkle@gmail.com>
2016-10-05 14:46:34 -0400

    fixed

--- js/script.js
+++ js/script.js
@@ -5,8 +5,18 @@ jQuery(document).ready(function($) {
     // mobile nav
     $('#menutoggle').click(function() {
         $('nav#primary, div#content').toggleClass('reveal');
-        $('i',this).toggleClass('menu_lines').toggleClass('close');
+        $('i', this).toggleClass('menu_lines').toggleClass('close');
 
+    });
+
+    //remove reveal class on #content when window is resized
+    $(window).resize(function() {
+        var viewportWidth = $(window).width();
+        if (viewportWidth > 1000) {
+            $("#content").removeClass("reveal");
+            $('nav#primary, div#content').removeClass("reveal");
+        $('#menutoggle i').addClass('menu_lines').removeClass('close');
+        }
     });
 
     // Check to see if the window is top if not then display button
