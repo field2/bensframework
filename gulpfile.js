0d3f71a5a9139bb0a12f64c85cffcfe8611084a5
Ben Dunkle <ben@field2.com>
2016-11-28 11:01:22 -0500

    Updating with new framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
@@ -9,6 +10,20 @@ var browserSync = require('browser-sync').create();
 var nunjucksRender = require('gulp-nunjucks-render');
 var svgSprite = require("gulp-svg-sprites");
 var sassOptions = { outputStyle: 'expanded' };
+=======
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
+    svgSprite = require("gulp-svg-sprites");
+//     var     handlebars = require('gulp-handlebars');
+//     var wrap = require('gulp-wrap');
+// var declare = require('gulp-declare');
+// var concat = require('gulp-concat');
+>>>>>>> Stashed changes
 
 gulp.task('watch', ['sass', 'nunjucks'], function() {
 browserSync.init({
@@ -22,6 +37,19 @@ browserSync.init({
     gulp.watch('./pages/*.html', ['nunjucks']);
 
 });
+
+gulp.task('templates', function(){
+  gulp.src('templates/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'MyApp.templates',
+      noRedeclare: true, // Avoid duplicate declarations 
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('js/'));
+});
+
 
 
 
@@ -70,5 +98,10 @@ gulp.task('sprites', function() {
 });
 
 
+<<<<<<< Updated upstream
 /* default task, runs with just gulp */
 gulp.task('default', ['watch']);
+=======
+
+gulp.task('default', ['watch', 'imagemin', 'sprites', 'templates']);
+>>>>>>> Stashed changes
